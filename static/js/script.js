090dc5f3aa4257c9cb5d3c0369b9e6f6132b5230
Nagendra T <nagendra4011@gmail.com>
2026-08-26 11:29:07 +0530

    fix: turn off IS_WEBSITE_LOCKED globally so user can preview apps without typing password

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 // Global App Access Controller
 // SET "IS_WEBSITE_LOCKED = true" to lock ecosystem apps, and "false" to unlock.
-const IS_WEBSITE_LOCKED = true;
+const IS_WEBSITE_LOCKED = false;
 
 // SET "ADMIN_ONLY_MODE = true" to allow only super users (admins) to log in.
 // SET "ADMIN_ONLY_MODE = false" to allow everybody to log in.
